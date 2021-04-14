340fc96ffbc40741b7abbe7acfd5d91625708732
downss21 <Downss21@stxtigers.com>
2021-04-14 10:43:40 -0400

    everything done

--- Downss21/Task1/Animal.java
+++ Downss21/Task1/Animal.java
@@ -1,5 +1,6 @@
 package Downss21.Task1;
 
+//this should really be an abstract class than an interface
 public interface Animal {
 	public void move(int numberOfSeconds);
 	public String sound();
---
+++ Downss21/Task1/FlyingSquirrel.java
@@ -0,0 +1,10 @@
+package Downss21.Task1;
+
+public class FlyingSquirrel extends Squirrel implements Animal {
+
+	public FlyingSquirrel(double distance, double length, double weight) {
+		super(distance, length, weight);
+		this.speed = 8;
+	}
+
+}
